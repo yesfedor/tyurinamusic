0b3151600a7926ee943be3c81cdb3717cd88a380
Fedor Garanin <yesfedor.go@gmail.com>
2025-02-28 20:48:12 +0300

    Scroll Improve

--- app/components/layout/provider/provider.vue
+++ app/components/layout/provider/provider.vue
@@ -1,9 +1,16 @@
-<script lang="ts" setup>
-
-</script>
-
 <template>
   <div class="app-layout-provider">
     <slot />
   </div>
 </template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss">
+.app-layout-provider {
+  max-width: 100vw;
+  overflow-x: hidden;
+}
+</style>
